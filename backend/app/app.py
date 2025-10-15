9ed42d71eddacee6985e5b899b574ddb0d4a6b42
catty <qeomtin11@gmail.com>
2025-10-15 17:36:17 +0900

    feat: 첫 config 작성과 db 생성 및 연결하기

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
 
     @app.route("/", methods=["GET"])
     def index():
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
-from app.app import create_app
+from app.app import create_app, db
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
