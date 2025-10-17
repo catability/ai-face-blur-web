6856edbdd2bae9d76795833773398578bfc8a9fc
catty <qeomtin11@gmail.com>
2025-10-17 23:48:02 +0900

    feat: deepsort 작성

--- backend/app/services/face_services.py
+++ backend/app/services/face_services.py
@@ -12,8 +12,12 @@ import math
 
 model = YOLO("model/yolov11n-face.pt")
 
+from deep_sort_realtime.deepsort_tracker import DeepSort
+tracker = DeepSort(max_age=15)
+
+
 def detect_faces(frame_dir, video, job):
-    tracker = Sort()
+    # tracker = Sort()
 
     last_per = 0
     preview_path = os.path.join(current_app.config["PREVIEWS_FOLDER"], f"{job.id}_preview.jpg")
@@ -24,16 +28,47 @@ def detect_faces(frame_dir, video, job):
         img = cv2.imread(frame_path)
         results = model(img)
 
-        detections = []
-        for box in results[0].boxes.xyxy.cpu().numpy():
-            x1, y1, x2, y2 = box[:4]
-            conf = box[4] if len(box) > 4 else 0.9
-            detections.append([x1, y1, x2, y2, conf])
+        # detections = []
+        # for box in results[0].boxes.xyxy.cpu().numpy():
+        #     x1, y1, x2, y2 = box[:4]
+        #     conf = box[4] if len(box) > 4 else 0.9
+        #     detections.append([x1, y1, x2, y2, conf])
 
-        tracked_objects = tracker.update(np.array(detections)) if len(detections) else []
+        # tracked_objects = tracker.update(np.array(detections)) if len(detections) else []
+        # bboxes = []
+
+        # for x1, y1, x2, y2, track_id in tracked_objects:
+        #     bboxes.append({
+        #         "x": int(x1), "y": int(y1),
+        #         "w": int(x2-x1), "h": int(y2-y1),
+        #         "id": int(track_id)
+        #     })
+
+        detections_for_deepsort = []
+        for result in results:
+            for box in result.boxes:
+                x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
+                conf = box.conf[0].cpu().numpy()
+                cls = box.cls[0].cpu().numpy()
+
+                w = x2 - x1
+                h = y2 - y1
+                detections_for_deepsort.append(
+                    ([int(x1), int(y1), int(w), int(h)], float(conf), int(cls))
+                )
+        
+        tracked_objects = tracker.update_tracks(detections_for_deepsort, frame=img)
+
         bboxes = []
+        
+        for track in tracked_objects:
+            if not track.is_confirmed():
+                continue
 
-        for x1, y1, x2, y2, track_id in tracked_objects:
+            track_id = track.track_id
+            ltrb = track.to_ltrb()
+            x1, y1, x2, y2 = ltrb
+
             bboxes.append({
                 "x": int(x1), "y": int(y1),
                 "w": int(x2-x1), "h": int(y2-y1),
@@ -85,8 +120,9 @@ def blur_faces(frames_dir, processed_frames_dir, video, job):
 
             face_region = img[y:y+h, x:x+w]
             if face_region.size > 0:
-                blurred = cv2.GaussianBlur(face_region, (51, 51), 30)
-                img[y:y+h, x:x+w] = blurred
+                # blurred = cv2.GaussianBlur(face_region, (51, 51), 30)
+                # img[y:y+h, x:x+w] = blurred
+                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 3)
         
         cv2.imwrite(output_path, img)
 
