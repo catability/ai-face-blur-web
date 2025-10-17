d0638c668e84447f4e656c9f1725cc8145115c2b
catty <qeomtin11@gmail.com>
2025-10-17 13:26:18 +0900

    feat: 영상 처리 테스크 함수 뼈대 작성

--- backend/app/services/video_services.py
+++ backend/app/services/video_services.py
@@ -1,4 +1,8 @@
 from threading import Thread
+from app.models import Video, Job
+from app.app import db
+
+import os
 
 
 def start_process_job(app, job_id):
@@ -6,4 +10,26 @@ def start_process_job(app, job_id):
     thread.start()
 
 def extract_and_detect_task(app, job_id):
-    pass
+    with app.app_context():
+        job = Job.query.get(job_id)
+        video = Video.query.get(job.video_id)
+
+        job.status = "running"
+        db.session.commit()
+
+        video_path = os.path.join(app.config["UPLOADS_FOLDER"], video.filename_stored)
+        frame_dir = os.path.join(app.config["FRAMES_FOLDER"], f"job_{job_id}")
+        os.makedirs(frame_dir, exist_ok=True)
+
+        try:
+            # extract frames
+            # detect faces
+
+            job.status = "completed"
+            job.progress = 100.0
+            db.session.commit()
+        except Exception as e:
+            job.status = "failed"
+            job.progress = 0.0
+            db.session.commit()
+            print("Error processing video:", e)
